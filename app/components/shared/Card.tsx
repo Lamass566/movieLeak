83ff4bfa098a1cebb33931f1bdaa8503777905c3
Misha Sysoev <54980483+Lamass566@users.noreply.github.com>
2024-01-03 17:26:50 +0200

    fixed bugs

--- app/components/shared/Card.tsx
+++ app/components/shared/Card.tsx
@@ -7,21 +7,25 @@ interface CardProps{
     poster:string;
     duration:number;
 }
+// /assets/posters/ferrari_ver3_xlg.jpg
 
 const Card:React.FC<CardProps> = ({
     title,
     year,
     genre,
     duration,
+    poster
 }) => {
     return(
-        <div className="w-full hidden gap-2 sm:flex sm:flex-col h-full relative">
-            <img src="/assets/posters/ferrari_ver3_xlg.jpg" className="rounded-2xl object-cover w-full" alt="" />
+        <div className="w-full  gap-2 sm:flex sm:flex-col h-full relative">
+            <img src={poster} className="rounded-2xl object-cover w-full" alt="" />
             <span className="absolute top-2 right-2 bg-white rounded-lg shadow-[0px_0px_29px_10px_#1A202C]"><p className="text-black p-1 font-bold">HD</p></span>  
-            <p className="text-light-1 px-2 font-bold">{title}</p>
-            <div className="flex justify-between px-2 opacity-[0.6] rounded-lg">
-                <p className="text-light-1 pt-[3px]">{year} - {duration}m</p>
-                <p className="text-light-1 border p-[2px] rounded-md">{genre}</p>
+            <div className="h-[100px] flex flex-col justify-between">
+                <p className="text-light-1 px-2 font-bold">{title}</p>
+                <div className="flex justify-between px-2 opacity-[0.6] rounded-lg">
+                    <p className="text-light-1 pt-[3px]">{year} - {duration}m</p>
+                    <p className="text-light-1 border p-[2px] rounded-md">{genre}</p>
+                </div>
             </div>
         </div>
     );
--- app/components/shared/Collection.tsx
+++ app/components/shared/Collection.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import Card from "./Card";
 
 interface CollectionProps{
@@ -9,24 +9,34 @@ interface CollectionProps{
 }
 
 const Collection: React.FC<CollectionProps> = ({title}) => {
+    const [movies, setMovies] = useState<any[]>([])
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOGY0MmE3ZjQ3MThhMzEzMDM0MmZmMTY5MjllZmVlNSIsInN1YiI6IjY1MDllZjFhZDZjMzAwMDEwZTA1Mzk1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oQvK2fZD6jVQGqcfL9d1W9ZVjhzQp1AqSLUpHaraaHw'
+        }
+      };
 
+      useEffect(()=>{
+        fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then((response) => {setMovies(response.results);console.log(response)})
+        .catch(err => console.error(err));
+      },[])
   return (
     <>
     <div className="mt-[60px] flex flex-col gap-2">
-        <div className="hidden sm:flex sm:justify-between">
+        <div className="flex justify-between">
             <p className=" text-light-1 text-[30px] font-bold flex items-center">{title}</p>
             <button className="bg-primary-500 rounded-xl p-2 px-10"><p className="text-light-1 font-bold">MORE</p></button>
         </div>
-        <div className="w-full flex flex-1 gap-2 min-[640px]:justify-between">
-            <Card title="Ferrari" genre="Movie" duration={103} year={2023} poster=""/>
-            <Card title="Ferrari" genre="Movie" duration={103} year={2023} poster=""/>
-            <Card title="Ferrari" genre="Movie" duration={103} year={2023} poster=""/>
-            <Card title="Ferrari" genre="Movie" duration={103} year={2023} poster=""/>
-            <div className="flex w-full bg-white h-[150px] items-center justify-center min-[640px]:hidden">
-                <p className="text-sm/[44px]">
-                    {title.toUpperCase()}
-                </p>
-            </div>
+        <div className="w-full grid grid-cols-2 min-[640px]:grid-cols-4 gap-2 min-[640px]:justify-between">
+          {
+            movies.slice(0, 4).map((item)=>(
+              <Card key={item.id} title={item.original_title} genre="Movie" duration={Math.floor(Math.random() * 200) + 100} year={item.release_date.substr(0, 4)} poster={`http://image.tmdb.org/t/p/original${item.poster_path}`}/>
+            ))
+          }
         </div>
     </div>
     
