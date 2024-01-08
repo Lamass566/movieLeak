c3ade217dd5391281a3ab7691024b06fdeed1b3d
Misha Sysoev <54980483+Lamass566@users.noreply.github.com>
2024-01-08 17:28:49 +0200

    added styles to sing up/in pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="absolute flex items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex-col">
+      <p className='text-heading3-bold mb-[50px] text-[50px] text-light-1 max-xs:hidden'>Movie<span className="text-primary-500">.Leak</span></p>
+        <SignIn />
+      <p className='text-heading3-bold mt-[50px] text-[50px] text-light-1 max-xs:hidden'>Movie<span className="text-primary-500">.Leak</span></p>
+    </div>
+  );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  <div className="absolute flex items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex-col">
+    <p className='text-heading3-bold mb-[50px] text-[50px] text-light-1 max-xs:hidden'>Movie<span className="text-primary-500">.Leak</span></p>
+      <SignUp />
+    <p className='text-heading3-bold mt-[50px] text-[50px] text-light-1 max-xs:hidden'>Movie<span className="text-primary-500">.Leak</span></p>
+  </div>
 }
