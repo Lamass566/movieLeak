fb40c4dfbc0c729e1d1b3f180c93087a49cd77a2
Misha Sysoev <54980483+Lamass566@users.noreply.github.com>
2024-01-16 23:49:07 +0200

    added detail page

--- app/(root)/detail-movie/[[...id]]/page.tsx
+++ app/(root)/detail-movie/[[...id]]/page.tsx
@@ -5,7 +5,8 @@ import { useParams} from "next/navigation";
 
 const Page = () => {
     const router = useParams().id;
-    
+    const [movie, setMovie] = useState<any>();
+    const [img, setImg] = useState<any>();
 
     // const [movies, setMovies] = useState<any[]>([])
     // const [page, setPage] = useState<number>(1)
@@ -17,17 +18,29 @@ const Page = () => {
         }
       };
 
-    //   useEffect(()=>{        
-    //     fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`, options)
-    //     .then(response => response.json())
-    //     .then((response) => {setMovies(response.results);console.log(response.results)})
-    //     .catch(err => console.error(err));
-    //   },[page])
+      useEffect(()=>{        
+        fetch(`https://api.themoviedb.org/3/movie/${router}?language=en-US`, options)
+        .then(response => response.json())
+        .then((response) => {setMovie(response); console.log(response)})
+        .catch(err => console.error(err));
+      },[])
+
+      useEffect(()=>{        
+        fetch(`https://api.themoviedb.org/3/movie/${router}/images`, options)
+        .then(response => response.json())
+        .then((response) => {setImg(response); console.log(response)})
+        .catch(err => console.error(err));
+      },[])
 
   return (
-    <p className="text-light-1">
-        {router}
-    </p>
+    // <p className="text-light-1">
+    //     {movie?.id}
+    // </p>
+    <div className="relative  w-full">
+      <img src={`http://image.tmdb.org/t/p/original${img?.backdrops[1].file_path}`} className="z-[1] object-cover" alt="" />
+      <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-b z-[1] from-transparent to-black"></div>
+      {/* <button className="bg-primary-500 absolute z-[22] left-5 bottom-1 rounded-xl p-2 px-10"><p className="text-light-1 font-bold">WATCH</p></button> */}
+    </div>
   )
 }
 
